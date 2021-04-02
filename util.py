b8e3725dd7aa48338f55c8b3f87fa2cf8e99cc41
niltonmalves <46911747+niltonmalves@users.noreply.github.com>
2021-04-02 10:50:10 -0300

    mult

--- util.py
+++ util.py
@@ -1,14 +1,21 @@
-
+import hashlib, sys, glob, argparse
 import os
+
 def read_txt(fname):
     f = open(fname, 'r')
     content = f.readlines()
     print(content)
     return content
 
+def sha1(fname):
+    sha1hash = hashlib.sha1()
+    with open(fname,errors='ignore') as handle: #opening the file one line at a time for memory considerations
+        for line in handle:
+            sha1hash.update(line.encode('utf-8'))
+    return(sha1hash.hexdigest())
 
-# for i in read_txt('output_files_matched_copy.txt'):
-#     os.remove(i.split('\n')[0])
 
-
+def look_files(path):
+    list_files = glob.glob(path+"\*")
+    return list_files
     
